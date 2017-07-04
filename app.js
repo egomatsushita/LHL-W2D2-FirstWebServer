d4707c841eecc4db28356ddf7a738a6642c5a7e9
Eduardo Go <eduardo.go.rj@gmail.com>
2017-07-04 15:43:40 +0000

    Add if statements to check the request.url

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const PORT = 8080
 
 // a function which handles requests and send response
 function requestHandlers(request, response) {
-  response.end(`Requested Path: ${request.url}\nRequested Method: ${request.method}`);
+  // response.end(`Requested Path: ${request.url}\nRequested Method: ${request.method}`);
+
+  if (request.url === '/') {
+    response.end("Welcome");
+  } else if (request.url === '/urls') {
+    response.end("www.lighthouselabs.ca\nwww.google.com");
+  } else {
+    response.statusCode = 404;
+    response.end("Unknown Path");
+  }
 }
 
 var server = http.createServer(requestHandlers);
